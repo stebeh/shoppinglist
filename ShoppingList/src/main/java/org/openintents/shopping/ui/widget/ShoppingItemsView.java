0b89b1960567016fef71efdda9753b385bf864ff
Stefan Behrens <stefan@nospam.com>
2017-12-15 10:07:07 +0100

    Toggle toggles also items that are REMOVED_FROM_LIST

--- ShoppingList/src/main/java/org/openintents/shopping/ui/widget/ShoppingItemsView.java
+++ ShoppingList/src/main/java/org/openintents/shopping/ui/widget/ShoppingItemsView.java
@@ -1251,7 +1251,7 @@ public class ShoppingItemsView extends ListView implements LoaderManager.LoaderC
                 doUpdate = (oldstatus == ShoppingContract.Status.BOUGHT);
             }
 
-            if (doUpdate) {
+            if (true) {
                 final ContentValues values = new ContentValues();
                 values.put(ShoppingContract.Contains.STATUS, newstatus);
                 if (debug) {
